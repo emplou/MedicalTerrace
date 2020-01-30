8cce38a03846794c637c1cf2369179677b6d46ad
mac.user42 <mac.user42@Kim.local>
2020-01-30 17:40:02 +0800

    Work on Edit function of Special Page

--- resources/views/admin/special_list.blade.php
+++ resources/views/admin/special_list.blade.php
@@ -19,7 +19,7 @@
                 <td>{!! $sp->sp_title_shldr !!}</td>
                 <td>{!! $sp->sp_ill_cat !!}</td>
                 <td>{!! $sp->sp_cat !!}</td>
-                <td><a href="#" class="btn btn-info overwrite_special" class="btn btn-info" style="background-color:#FCB0BC;border-color:#FCB0BC;" sp-id="{!! $sp->id !!}">病院登録<br>Overwrite Editing</a> <a href="#" class="btn btn-success" style="background-color:#6ebaba;border-color:#6ebaba;">コピーと新しい編集<br>Copy and new edit</a></td>
+                <td><a href="#" class="btn btn-info overwrite_special" style="background-color:#FCB0BC;border-color:#FCB0BC;" sp-id="{!! $sp->id !!}">病院登録<br>Overwrite Editing</a> <a href="#" class="btn btn-success" style="background-color:#6ebaba;border-color:#6ebaba;">コピーと新しい編集<br>Copy and new edit</a></td>
             </tr>
         @endforeach
         </tbody>
