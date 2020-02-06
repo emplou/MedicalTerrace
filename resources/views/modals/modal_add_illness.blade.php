90697d12db8b0817dc0c3060df522fd68c45d341
mac.user46 <mac.user46@Suis-Macbook-Pro.local>
2020-02-06 18:01:51 +0800

    Suissa : Preview Illness Page

--- resources/views/modals/modal_add_illness.blade.php
+++ resources/views/modals/modal_add_illness.blade.php
@@ -1,206 +1,160 @@
 <div id="previewAddIllness" class="modalDialog">
     <div>	
+
         <a href="#close" title="Close" class="close">X</a>
         <div class="modal-panel">
+            <ul class="breadcrumb">
+                <li><a href="#"><img src="images/icon-home-green.png"></a></li>
+                <li><a href="#">感染症・寄生虫  </a></li>
+                <li>手足口病</a></li>
+            </ul>
             <div class="panel-head">感染症・寄生虫</div>
             <div class="div-bg">
-                <div class="heading1">手・足・口に赤い発疹やみずぶくれが出ます<br>手足口病</div>
-                <div class="date">2019.06.21　Medical T. 編集部 A.Ito</div>
+                <div class="heading1">手・足・口に赤い発疹やみずぶくれが出ます<br>
+                <span>てあしくちびょう</span><br>手足口病</div>
+            </div>
+            <div class="panel-pads">
+                    <div class="date">2019.06.21　Medical T. 編集部 A.Ito</div>
+                    <div class="tags">
+                        <span>子ども</span>
+                        <span>夏</span>
+                        <span>感染症</span>
+                        <span>夏風邪</span>
+                        <span>手足口病</span>
+                    </div>
+                    <div class="tooltip">この記事をまとめると</div>
+                    <ul class="checks">
+                        <li>乳幼児、特に集団生活をしている子どもは注意。</li>
+                        <li>手と足、口に赤い発疹や水ぶくれができます。</li>
+                        <li>７月に流行のピークを迎えます。</li>
+                        <li>高熱はあまり出ません。</li>
+                        <li>石けんでの手洗い、うがいをして予防します。</li>
+                    </ul>
+                    <ol class="link">
+                        <li><a href="">手足口病の基礎知識</a></li>
+                        <li><a href="">手足口病の近年の動向</a></li>
+                        <li><a href="">手足口病の症状</a></li>
+                        <li><a href="">手足口病の原因</a></li>
+                        <li><a href="">手足口病の治療方法</a></li>
+                        <li><a href="">手足口病の合併症</a></li>
+                        <li><a href="">手足口病の自宅療法（療養方法、再発防止など)</a></li>
+                        <li><a href="">手足口病の予防・対策方法</a></li>
+                        <li><a href="">手足口病のリスク度チェック</a></li>
+                        <li><a href="">手足口病のセルフチェック</a></li>
+                    </ol>
+                    <h3>手足口病の基礎知識</h3>
+                    </p>主に夏に流行する感染症で、3〜6日の潜伏期の後、手足などに赤い発疹や水ぶくれ、口内炎ができます。高熱が出ることは少なく、1週間ほどで自然に治ります。かかりやすい年齢は5歳以下。毎年、5月頃から患者数が増え、7月に流行のピークを迎えます。一般的には夏風邪と呼ばれることもあります。全身状態が改善すれば、登園・登校が可能になります。<p>
+
+                    <strong>[2013年〜2018年の定点当たり報告数の平均] 手足口病</strong>
+                    <img class="graph" src="images/illness-graph.png" alt="">
+                    </p>※1 データの週によるばらつきや未報告のデータを修正するために統計処理をしたもの。+1SDの数値 は過去のデータの68 ％、+2SDの数値は過去のデータの95 ％が含まれる。
+                    <br>※2 過去5年間の平均：前週、当該週、後週の合計15週の平均<br>◎国立感染症研究所のデータより<p>
+                    
+                    <h3>手足口病の近年の動向</h3>
+                    </p>国立感染症研究所がまとめた患者報告によると、2019年6月は、過去10年の同期と比べて最多になりました。<p>
+
+                    <h3>手足口病の症状</h3>
+                    <p><span>手のひらや足の裏に赤い発疹</span>や<span>水ぶくれ</span>ができます。口の中にも赤い発疹や水ぶくれ（<span>口内炎</span>)ができます。次第に、手足全体、ひじ、ひざ、おしりに出ることもあります。高熱が出ることはあまりなく、発病しても、<span>軽い症状</span>だけで治ることが多くあります。また、夏風邪と言われますが、冬の風とは異なり、鼻水や咳の症状はあまりありません。</p>
+
+                    <h3>手足口病の原因</h3>
+                    </p><span>エンテロウイルス属のウイルス</span>による感染です。具体的にはコクサッキーウイルスA6、A16、エンテロウイルス71（EV71）などが挙げられます。感染経路には、<span>飛沫感染、接触感染、糞口感染</span>（便の中に排泄されたウイルスが口に入って感染する）があります。特に、保育施設や幼稚園など、乳幼児が<span>集団生活</span>をしている施設では注意が必要です。ウイルスの潜伏期があるため、施設で手足口病にかかった子どもがいると、集団感染が起こりやすくなります。原因となるウイルスに初めて感染する乳幼児がいると、感染経験がないため発病しやすくなります。<p>
+
+                    <h3>手足口病の合併症</h3>
+                    </p>まれですが、髄膜炎、小脳失調症、脳炎などの中枢神経系の症状のほか、心筋炎、神経原性肺水腫、急性弛緩性麻痺など、さまざまな合併症が起こる場合があります。<p>
+
+                    <h3>手足口病の自宅療法（療養方法、再発防止など）</h3>
+                    </p>口内炎があってうまく食事ができない場合は柔らかく、味の薄い、冷たい食事にします。食欲が落ちている時はこまめに水分をとるようにしましょう。<p>
+
+                    <h3>手足口病の予防・対策方法</h3>
+                    </p>有効なワクチンはありません。発病を予防する薬もありません。接触感染を予防するために、しっかり手洗いをすること、うがいをすることなどが基本の対策になります。手洗いは石けんを使い、流水で洗いましょう。治った後も2〜4週間ほど、ウイルスが便中に排泄されることがあるため、感染を広げないために、排泄物を適切に処理することが必要です。おむつ交換をする時は特に気をつけ、しっかり手を洗います。タオルの共用もしません。家庭では兄弟への感染に気をつけること、乳幼児の保育施設などの集団生活では、職員と子どもがしっかり対策をすることが大切です。<p>
+                </div>   
+                <div class="panel-pink">
+                    <h3>手足口病のリスク度チェック</h3>
+                
+                    <div class="form-group check">
+                        <ul>    
+                            <li>
+                                <input class="styled-checkbox" id="1a" type="checkbox" name="1a" >
+                                <label for="1a" style="font-weight:500;">5歳以下の乳幼児</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="2a" type="checkbox" name="2a" >
+                                <label for="2a" style="font-weight:500;">乳幼児の保育施設などで、普段から集団生活をしている</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="3a" type="checkbox" name="3a" >
+                                <label for="3a" style="font-weight:500;">手洗いやうがいはあまりしない</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="4a" type="checkbox" name="4a" >
+                                <label for="4a" style="font-weight:500;">周囲で手足口病やヘルパンギーナにかかった子ども、人がいる</label>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="panel-pink">
+                    <h3>手足口病のセルフチェック</h3>
+                    <strong>予防・対策はしっかりできていますか？</strong>
+                    <div class="form-group check">
+                        <ul>    
+                            <li>
+                                <input class="styled-checkbox" id="1b" type="checkbox" name="1b" >
+                                <label for="1b" style="font-weight:500;">手洗いうがいをしっかり行う</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="2b" type="checkbox" name="2b" >
+                                <label for="2b" style="font-weight:500;">子どもの排便、おむつ替えなどの処理を適正に行っている</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="3b" type="checkbox" name="3b" >
+                                <label for="3b" style="font-weight:500;">タオルは共用していない</label>
+                            </li>
+                        </ul>
+                        <strong>下記のような症状はありますか？</strong>
+                        <ul>
+                            <li>
+                                <input class="styled-checkbox" id="4b" type="checkbox" name="4b" >
+                                <label for="4b" style="font-weight:500;">手のひらや足の裏に赤い湿疹、または水ぶくれができている</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="5b" type="checkbox" name="5b" >
+                                <label for="5b" style="font-weight:500;">口内炎がある</label>
+                            </li>
+                            <li>
+                                <input class="styled-checkbox" id="6b" type="checkbox" name="6b" >
+                                <label for="6b" style="font-weight:500;">全身がだるい</label>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="panel-blue">
+                    <div class="id-tag">
+                        監修・取材協力<br>
+                        日本小児科学会認定小児科専門医・日本ア<br>
+                        レルギー学会認定専門医<br>
+                        医学博士 浅野 勉<br>
+                        <a href="">詳しくはこちら</a>
+                    </div>
+
+                    
+                    <p></p>
+                </div>
             </div>
             <div class="tags">
-                <span>子ども</span>
-                <span>夏</span>
-                <span>感染症</span>
-                <span>夏風邪</span>
-                <span>手足口病</span>
+                <div>
+                    <span>子ども</span>
+                    <span>夏</span>
+                    <span>感染症</span>
+                    <span>夏風邪</span>
+                    <span>手足口病</span>
+                </div>
             </div>
-            <div class="tooltip">この記事をまとめると</div>
-            <ul>
-                <li>乳幼児、特に集団生活をしている子どもは注意。</li>
-                <li>手と足、口に赤い発疹や水ぶくれができます。</li>
-                <li>７月に流行のピークを迎えます。</li>
-                <li>高熱はあまり出ません。</li>
-                <li>石けんでの手洗い、うがいをして予防します。</li>
-            </ul>
-            <ol class="link">
-                <li>手足口病の基礎知識</li>
-                <li>手足口病の近年の動向</li>
-                <li>手足口病の症状</li>
-                <li>手足口病の原因</li>
-                <li>手足口病の治療方法</li>
-                <li>手足口病の合併症</li>
-                <li>手足口病の自宅療法（療養方法、再発防止など）</li>
-                <li>手足口病の予防・対策方法</li>
-                <li>手足口病のリスク度チェック</li>
-                <li>手足口病のセルフチェック</li>
-            </ol>
-            <h3>手足口病の基礎知識</h3>
-            </p>主に夏に流行する感染症で、3〜6日の潜伏期の後、手足などに赤い発疹や水ぶくれ、口内炎ができます。高熱が出ることは少なく、1週間ほどで自然に治ります。かかりやすい年齢は5歳以下。毎年、5月頃から患者数が増え、7月に流行のピークを迎えます。一般的には夏風邪と呼ばれることもあります。全身状態が改善すれば、登園・登校が可能になります。<p>
-
-            <h3>【2013年〜2018年の定点当たり報告数の平均】手足口病</h3>
-            <img src="images/illness-graph.png" alt="">
-            </p>※1 データの週によるばらつきや未報告のデータを修正するために統計処理をしたもの。+1SDの数値 は過去のデータの68 ％、+2SDの数値は過去のデータの95 ％が含まれる。
-            <br>※2 過去5年間の平均：前週、当該週、後週の合計15週の平均<br>◎国立感染症研究所のデータより<p>
-            
-            <h3>手足口病の近年の動向</h3>
-            </p>国立感染症研究所がまとめた患者報告によると、2019年6月は、過去10年の同期と比べて最多になりました。<p>
-
-            <h3>手足口病の症状</h3>
-            </p>手のひらや足の裏に赤い発疹や水ぶくれができます。口の中にも赤い発疹や水ぶくれ（口内炎）ができます。次第に、手足全体、ひじ、ひざ、おしりに出ることもあります。高熱が出ることはあまりなく、発病しても、軽い症状だけで治ることが多くあります。また、夏風邪と言われますが、冬の風とは異なり、鼻水や咳の症状はあまりありません。<p>
-
-            <h3>手足口病の原因</h3>
-            </p>エンテロウイルス属のウイルスによる感染です。具体的にはコクサッキーウイルスA6、A16、エンテロウイルス71（EV71）などが挙げられます。感染経路には、飛沫感染、接触感染、糞口感染（便の中に排泄されたウイルスが口に入って感染する）があります。特に、保育施設や幼稚園など、乳幼児が集団生活をしている施設では注意が必要です。ウイルスの潜伏期があるため、施設で手足口病にかかった子どもがいると、集団感染が起こりやすくなります。原因となるウイルスに初めて感染する乳幼児がいると、感染経験がないため発病しやすくなります。<p>
-
-            <h3>手足口病の合併症</h3>
-            </p>まれですが、髄膜炎、小脳失調症、脳炎などの中枢神経系の症状のほか、心筋炎、神経原性肺水腫、急性弛緩性麻痺など、さまざまな合併症が起こる場合があります。<p>
-
-            <h3>手足口病の自宅療法（療養方法、再発防止など）</h3>
-            </p>口内炎があってうまく食事ができない場合は柔らかく、味の薄い、冷たい食事にします。食欲が落ちている時はこまめに水分をとるようにしましょう。<p>
-
-            <h3>手足口病の予防・対策方法</h3>
-            </p>有効なワクチンはありません。発病を予防する薬もありません。接触感染を予防するために、しっかり手洗いをすること、うがいをすることなどが基本の対策になります。手洗いは石けんを使い、流水で洗いましょう。治った後も2〜4週間ほど、ウイルスが便中に排泄されることがあるため、感染を広げないために、排泄物を適切に処理することが必要です。おむつ交換をする時は特に気をつけ、しっかり手を洗います。タオルの共用もしません。家庭では兄弟への感染に気をつけること、乳幼児の保育施設などの集団生活では、職員と子どもがしっかり対策をすることが大切です。<p>
-            
-            <h3>手足口病のリスク度チェック</h3>
            
-            <div class="form-group check">
-                <ul>    
-                    <li>
-                        <input class="styled-checkbox" id="1a" type="checkbox" name="1a" >
-                        <label for="1a" style="font-weight:500;">5歳以下の乳幼児</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="2a" type="checkbox" name="2a" >
-                        <label for="2a" style="font-weight:500;">乳幼児の保育施設などで、普段から集団生活をしている</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="3a" type="checkbox" name="3a" >
-                        <label for="3a" style="font-weight:500;">手洗いやうがいはあまりしない</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="4a" type="checkbox" name="4a" >
-                        <label for="4a" style="font-weight:500;">周囲で手足口病やヘルパンギーナにかかった子ども、人がいる</label>
-                    </li>
-                </ul>
-            </div>
-
-            <h3>手足口病のセルフチェック</h3>
-            <strong>予防・対策はしっかりできていますか？</strong>
-            <div class="form-group check">
-                <ul>    
-                    <li>
-                        <input class="styled-checkbox" id="1b" type="checkbox" name="1b" >
-                        <label for="1b" style="font-weight:500;">手洗いうがいをしっかり行う</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="2b" type="checkbox" name="2b" >
-                        <label for="2b" style="font-weight:500;">子どもの排便、おむつ替えなどの処理を適正に行っている</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="3b" type="checkbox" name="3b" >
-                        <label for="3b" style="font-weight:500;">タオルは共用していない</label>
-                    </li>
-                </ul>
-                <strong>下記のような症状はありますか？</strong>
-                <ul>
-                    <li>
-                        <input class="styled-checkbox" id="4b" type="checkbox" name="4b" >
-                        <label for="4b" style="font-weight:500;">手のひらや足の裏に赤い湿疹、または水ぶくれができている</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="5b" type="checkbox" name="5b" >
-                        <label for="5b" style="font-weight:500;">口内炎がある</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="6b" type="checkbox" name="6b" >
-                        <label for="6b" style="font-weight:500;">全身がだるい</label>
-                    </li>
-                </ul>
-            </div>
-            <div class="id-tag">
-                監修・取材協力<br>
-                日本小児科学会認定小児科専門医・日本ア<br>
-                レルギー学会認定専門医<br>
-                医学博士 浅野 勉<br>
-                <a href="">詳しくはこちら</a>
-            </div>
-
-            <div class="tags">
-                <span>子ども</span>
-                <span>夏</span>
-                <span>感染症</span>
-                <span>夏風邪</span>
-                <span>手足口病</span>
-            </div>
-            <p></p>
-
-            <!-- TBD -->
-            <h2>医師・医療従事者</h2>
-             <p>|岐阜|岐阜市|小児科|アレルギー科|新生児内科|</p>
-             <div class="bg">
-                <p>日本小児科学会認定小児科専門医・日本アレルギー学会 認定専門医 医学博士</p>
-                <p>浅野 勉 -Tsutomu Asano-</p>
-                <p>2019.06.21</p>
-            </div>
-             <img src="images/avatar.jpg" alt="">
-             <ul>
-                 <li>所属学会 日本小児科学会・日本アレルギー学会・日本小児アレル ギー学会・日本新生児成育医学会</li>
-                 <li>市立長浜病院小児科・NICU 部長 岐阜大学医学部附属病院新生児集中治療部 臨床講師 2015年9月「のりたけキッズベビークリニック」開院 現在に至る</li>
-                 <li>コメント 医師プロフィールページに掲載されるコメントをお願 いします。この文章はダミーです。文字の大きさ、量、字 間、行間等を確認するために入れています。この文章は ダミーです。文字の大きさ、量、字間、行間等を確認する ために入れています。この文章はダミーです。文字の大 きさ、量、字間、行間等を確認するために入れています。</li>
-                 <li>コメント 医師プロフィールページに掲載されるコメントをお願 いします。この文章はダミーです。文字の大きさ、量、字 間、行間等を確認するために入れています。この文章は ダミーです。文字の大きさ、量、字間、行間等を確認する ために入れています。この文章はダミーです。文字の大 きさ、量、字間、行間等を確認するために入れています。</li>
-            </ul> 
-
-            <h3>手足口病の基礎知識</h3>
-            </p>主に夏に流行する感染症で、3〜6日の潜伏期の後、手足などに赤い発疹や水ぶくれ、口内炎ができます。高熱が出ることは少なく、1週間ほどで自然に治ります。かかりやすい年齢は5歳以下。毎年、5月頃から患者数が増え、7月に流行のピークを迎えます。一般的には夏風邪と呼ばれることもあります。全身状態が改善すれば、登園・登校が可能になります。<p>
-
-            <h3>【2013年〜2018年の定点当たり報告数の平均】手足口病</h3>
-            <img src="images/illness-graph.png" alt="">
-            </p>※1 データの週によるばらつきや未報告のデータを修正するために統計処理をしたもの。+1SDの数値 は過去のデータの68 ％、+2SDの数値は過去のデータの95 ％が含まれる。
-            <br>※2 過去5年間の平均：前週、当該週、後週の合計15週の平均<br>◎国立感染症研究所のデータより<p>
-            
-            <h3>手足口病の近年の動向</h3>
-            </p>国立感染症研究所がまとめた患者報告によると、2019年6月は、過去10年の同期と比べて最多になりました。<p>
-
-            <h3>手足口病の症状</h3>
-            </p>手のひらや足の裏に赤い発疹や水ぶくれができます。口の中にも赤い発疹や水ぶくれ（口内炎）ができます。次第に、手足全体、ひじ、ひざ、おしりに出ることもあります。高熱が出ることはあまりなく、発病しても、軽い症状だけで治ることが多くあります。また、夏風邪と言われますが、冬の風とは異なり、鼻水や咳の症状はあまりありません。<p>
-
-            <h3>手足口病の原因</h3>
-            </p>エンテロウイルス属のウイルスによる感染です。具体的にはコクサッキーウイルスA6、A16、エンテロウイルス71（EV71）などが挙げられます。感染経路には、飛沫感染、接触感染、糞口感染（便の中に排泄されたウイルスが口に入って感染する）があります。特に、保育施設や幼稚園など、乳幼児が集団生活をしている施設では注意が必要です。ウイルスの潜伏期があるため、施設で手足口病にかかった子どもがいると、集団感染が起こりやすくなります。原因となるウイルスに初めて感染する乳幼児がいると、感染経験がないため発病しやすくなります。<p>
-
-            <h3>手足口病の合併症</h3>
-            </p>まれですが、髄膜炎、小脳失調症、脳炎などの中枢神経系の症状のほか、心筋炎、神経原性肺水腫、急性弛緩性麻痺など、さまざまな合併症が起こる場合があります。<p>
-
-            <h3>手足口病の自宅療法（療養方法、再発防止など）</h3>
-            </p>口内炎があってうまく食事ができない場合は柔らかく、味の薄い、冷たい食事にします。食欲が落ちている時はこまめに水分をとるようにしましょう。<p>
-
-            <h3>手足口病の予防・対策方法</h3>
-            </p>有効なワクチンはありません。発病を予防する薬もありません。接触感染を予防するために、しっかり手洗いをすること、うがいをすることなどが基本の対策になります。手洗いは石けんを使い、流水で洗いましょう。治った後も2〜4週間ほど、ウイルスが便中に排泄されることがあるため、感染を広げないために、排泄物を適切に処理することが必要です。おむつ交換をする時は特に気をつけ、しっかり手を洗います。タオルの共用もしません。家庭では兄弟への感染に気をつけること、乳幼児の保育施設などの集団生活では、職員と子どもがしっかり対策をすることが大切です。<p>
-            
-            <h3>手足口病のリスク度チェック</h3>
-           
-            <div class="form-group check">
-                <ul>    
-                    <li>
-                        <input class="styled-checkbox" id="1a" type="checkbox" name="1a" >
-                        <label for="1a" style="font-weight:500;">5歳以下の乳幼児</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="2a" type="checkbox" name="2a" >
-                        <label for="2a" style="font-weight:500;">乳幼児の保育施設などで、普段から集団生活をしている</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="3a" type="checkbox" name="3a" >
-                        <label for="3a" style="font-weight:500;">手洗いやうがいはあまりしない</label>
-                    </li>
-                    <li>
-                        <input class="styled-checkbox" id="4a" type="checkbox" name="4a" >
-                        <label for="4a" style="font-weight:500;">周囲で手足口病やヘルパンギーナにかかった子ども、人がいる</label>
-                    </li>
-                </ul>
-            </div>
-
-            
             <!-- Buttons -->
-        <button type="button" class="btn-prime approval" id="show_img"></button>
+            <div class="modal-buts">
+                <button type="button" class="btn-prime approval" id="show_img"></button>
+                <button type="button" class="btn-prime backedit" id="show_img"></button>
+            </div>
     
         </div>
     </div>
