076aacf88b194bb0fc133848667e4caa85366e8b
mac.user46 <mac.user46@Suis-Macbook-Pro.local>
2020-02-18 17:57:49 +0800

    Suissa: Continue Hospital2

--- resources/views/admin/add_hospital.blade.php
+++ resources/views/admin/add_hospital.blade.php
@@ -204,7 +204,7 @@
     </div>
 
     
-
+    <div class="field_wrap1_hosp"></div>
     <!-- Subheading -->
     <div class="form-group">
         <label class="control-label cols-15"> 医療機関キャッチ<br><span>Subheading</span></label>
@@ -222,7 +222,7 @@
         <label class="control-label cols-15">本文<br><span>Text of Subheading</span></label>
         <div class="cols-6">   
             <!-- add new textarea here -->
-            <div class="field_wrap1_hosp"></div>
+            
             <div id="addanother1">
                 <div class="cols-10">        
                     <textarea class="form-control" name="text_subheading_hospital" id="txt_ckeditor">メイン写真の直下に入るリードの部分です。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</textarea>
@@ -322,6 +322,7 @@
         </div>
     </div>
 
+    <div class="field_wrap2_hosp"></div>
     <!-- Medical Subject -->
     <div class="form-group">
         <label class="control-label cols-15">診療科目<br><span>Medical Subject</span></label>
@@ -350,7 +351,7 @@
         <label class="control-label cols-15">本文<br><span>Text of Subheading</span></label>
         <div class="cols-6">   
             <!-- add new textarea here -->
-            <div class="field_wrap2_hosp"></div>
+            
             <div id="addanother1">
                 <div class="cols-10">        
                     <textarea class="form-control" name="med_subj_text_subheading_hospital" id="txt_ckeditor">メイン写真の直下に入るリードの部分です。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</textarea>
@@ -437,6 +438,7 @@
 
      <hr /> 
     <!-- Subheading 3-->
+    <div class="field_wrap3_hosp"></div>
     <div class="form-group">
         <label class="control-label cols-15"> 医療機関キャッチ<br><span>Subheading</span></label>
         <div class="cols-5">          
@@ -448,13 +450,11 @@
         </div>
     </div>
 
-
     <!-- Text of Subheading 3-->
     <div class="form-group editor">
         <label class="control-label cols-15">本文<br><span>Text of Subheading</span></label>
         <div class="cols-6">   
             <!-- add new textarea here -->
-            <div class="field_wrap3_hosp"></div>
             <div id="addanother1">
                 <div class="cols-10">        
                     <textarea class="form-control" name="feature_text_subheading_hospital" id="txt_ckeditor">メイン写真の直下に入るリードの部分です。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</textarea>
@@ -462,7 +462,9 @@
             </div>
         </div>
         <div class="cols-1 relative"><button type="button" class="btn btn-success addhosp3"><span class="lnr lnr-plus-circle"></span></button></div> 
+   
     </div>
+    
 
      <!-- Buttons -->
     <div class="form-group">
@@ -478,6 +480,7 @@
 
     <hr />
     <!-- Subheading 4-->
+    <div class="field_wrap4_hosp"></div>
     <div class="form-group">
         <label class="control-label cols-15"> 設備・機器<br><span>Equipment</span></label>
         <div class="cols-5">          
@@ -494,7 +497,7 @@
         <label class="control-label cols-15"> </label>
         <div class="cols-6">   
             <!-- add new textarea here -->
-            <div class="field_wrap4_hosp"></div>
+            
             <div id="addanother1">
                 <div class="cols-10">        
                     <textarea class="form-control" name="equipment_subheading2" id="txt_ckeditor">メイン写真の直下に入るリードの部分です。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</textarea>
@@ -527,6 +530,8 @@
     </div>
 
     <hr />
+
+    <div class="field_wrap5_hosp"></div>
     <!-- Subheading 4-->
     <div class="form-group">
         <label class="control-label cols-15"> スタッフリード<br><span>Staff subheading</span></label>
@@ -537,12 +542,13 @@
         
     </div>
 
+    
     <!-- Text of Subheading 4-->
     <div class="form-group editor">
          <label class="control-label cols-15"> スタッフコメント<br><span>Staff Comment</span></label>
         <div class="cols-6">   
             <!-- add new textarea here -->
-            <div class="field_wrap5_hosp"></div>
+            
             <div id="addanother1">
                 <div class="cols-10">        
                     <textarea class="form-control" name="staff_comment_hospital" id="txt_ckeditor">メイン写真の直下に入るリードの部分です。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。</textarea>
@@ -657,6 +663,7 @@
     </div>
 
     <!-- Number of Beds -->    
+    <div class="field_wrap6_hosp"></div>
     <div class="form-group">
         <div class="control-label cols-15">病床種別・病床数<br><span>Number of beds</span></div>
         <div class="cols-05">
@@ -677,6 +684,7 @@
     </div>
 
     <!-- Number of Beds -->    
+    <div class="field_wrap7_hosp"></div>
     <div class="form-group check fix">
         <div class="control-label cols-15">面会可能日・時間<br><span>Possible date of visit</span></div>
         <div class="cols-1">
