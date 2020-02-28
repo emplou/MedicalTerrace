e5504cf998d6219c3b84fb2169f731cbf5771bca
mac.user46 <mac.user46@Suis-Macbook-Pro.local>
2020-02-28 17:26:07 +0800

    Suissa Updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,9 +45,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index(){
+        $doctors = DB::table('dv_doctors')->get();
+        return view('home', compact('doctors'));
     }
 
     public function add_doctor(){
