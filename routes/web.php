77fda96bf0e10e8b3058dc49198775ae6e64b707
mac.user42 <mac.user42@Kim.local>
2020-03-12 17:21:48 +0800

    Kim's Update

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::get('/modal_preview_illness','HomeController@modal_preview_illness');
 
 //edit
 Route::get('/edit_illness/{id}','HomeController@edit_illness');
+Route::get('/edit_special/{id}','HomeController@edit_special');
 
 //post
 Route::post('/save_hospital','HomeController@save_hospital');
